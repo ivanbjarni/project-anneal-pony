af90a18a4d39ddffeed7537e307c881d66e563e3
Ivan <ivan@ivan-SATELLITE-C660.(none)>
2014-01-23 11:25:35 +0000

    fixed bug

--- pythTrip.py
+++ pythTrip.py
@@ -3,6 +3,6 @@ golden = input("What kinda' Apples do you like?\n")
 for a in xrange(0,golden):
 	for b in xrange(a,golden-a):
 		c = golden - a -b ;
-		if(a**2+b**2-c**2==0):
+		if(a**2+b**2-c**2==1):
 			print str(a*b*c);
 			#heheheheheh
