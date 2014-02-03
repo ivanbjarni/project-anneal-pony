2a236a679f979fac7d82994ae2f5daf34e291d09
mister09 <ahh19@hi.is>
2014-02-03 19:30:12 +0000

    bs4 farid, verdbolguspa komin

---
+++ inflation.py
@@ -0,0 +1,36 @@
+# -*- coding: cp1252 -*-
+#Notkun: x = readInflation(visitala.txt)
+#Fyrir: visitala.txt er textaskrá sem inniheldur visitolur manadanna 
+#       januar 1990 til januar 2014
+#Eftir: x er listi vísitalna í öfugri lesröð(1. janúar er fremstur)
+def readInflation(files):
+        f = open(files,'r')
+       # strings = f.read()
+        listi = []
+        
+        done = False
+        while(not done):
+                s = f.readline()
+                if(s != ''):
+                        listi.append(s)
+                else:
+                        done = True
+        #for item in listi:
+         #       print item
+        rightlisti = []
+        for item in listi:
+                rightlisti.insert(0,item)
+                
+        return rightlisti
+#Notkun:x = getInflationCoefficient(months)
+#Fyrir: months er fjöldi mánaða sem við ætlum að nota
+#Eftir: x er verðbólguspá reiknuð með  vísitölum months marga mánuði aftur í
+#       tímann. Þetta er meðaltalsspá.
+def getInflationCoefficient(months):
+        listi = readInflation('visitala.txt')
+        i=0
+        summa = 0
+        while(i<=months):
+                summa+=float(listi[i])
+                i+=1
+        return summa/i
